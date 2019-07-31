b937f98299418faca141451f73db2d01302921c8
yeyoan <yeyoan@gmail.com>
2019-07-31 21:39:10 +0800

    Updates

---
+++ precache-manifest.e6c99c4767b3b9ea21e5b68c817b2974.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc6292a99ee6c23c30c989469ab86402",
+    "url": "/noto/index.html"
+  },
+  {
+    "revision": "6bb1958bf3db32ecb16d",
+    "url": "/noto/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "fbc27956612454249634",
+    "url": "/noto/static/js/2.f94bf866.chunk.js"
+  },
+  {
+    "revision": "6bb1958bf3db32ecb16d",
+    "url": "/noto/static/js/main.74ee03be.chunk.js"
+  },
+  {
+    "revision": "f7bf74704f78d5c3da37",
+    "url": "/noto/static/js/runtime~main.6a77357a.js"
+  }
+]);
